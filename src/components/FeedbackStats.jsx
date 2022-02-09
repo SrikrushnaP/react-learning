5a56fe18cd3b3fdaf6b5e9fb306151e3be4ec310
Srikrushna <palsrikrushna6@gmail.com>
2022-02-09 21:31:38 +0530

    Added delete functionality and average rating

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 function FeedbackStats({feedback}) {
+
+    let average = feedback.reduce( (acc, cur) => {
+        return acc + cur.rating
+    }, 0) / feedback.length
+
+    average = average.toFixed(1).replace(/[.,]0$/, '');
+
   return (<>
     <div className='feedback-stats'>
         <h4>{feedback.length} Review</h4>
-        <h4>Average Rating: {5}</h4>
+        <h4>Average Rating: {average}</h4>
     </div>
   </>);
 }
