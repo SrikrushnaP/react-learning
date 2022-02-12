bdf1a4cf6100b0317422c5200ed08952cda8dc6c
Srikrushna <palsrikrushna6@gmail.com>
2022-02-12 12:23:46 +0530

    Added validation and prevet defult form submition

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,15 +3,31 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 
 function FeedbackForm() {
-    const [text, setText] = useState('');
+    const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+          } else if (text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters')
+            setBtnDisabled(true)
+          } else {
+            setMessage(null)
+            setBtnDisabled(false)
+          }
         setText(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
     }
 
   return (
     <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us ?</h2>
             <div className='input-group'>
                 <input
@@ -20,8 +36,10 @@ function FeedbackForm() {
                     placeholder='Write a review'
                     value={text}
                 />
-                <Button type='submit'>Send</Button>
+                <Button type='submit' isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
